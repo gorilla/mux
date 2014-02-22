4a88682a46c202995ccfccb1707e3668c7dac397
tarrsalah <tarrsalah@gmail.com>
2014-02-22 21:58:07 +0100

    First mux usage, the simplest case ever.

--- exp/main.go
+++ exp/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"github.com/tarrsalah/mux"
+	"net/http"
 )
 
+const (
+	http_port = ":8080"
+)
+
+var root *mux.Router
+
+func E1() {
+	root = mux.NewRouter()
+	root.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello gopher gorilla !"))
+	})
+}
+
 func main() {
-	fmt.Println("hello gorilla")
+	E1()
+	http.Handle("/", root)
+	http.ListenAndServe(http_port, nil)
 }
