13c8226081008f7ade9c619da744ed14e78eb8de
Orne Brocaar <info@brocaar.com>
2015-08-05 10:24:37 +0200

    Update comment and specify KeepContext option.

--- mux.go
+++ mux.go
@@ -312,9 +312,10 @@ func Vars(r *http.Request) map[string]string {
 }
 
 // CurrentRoute returns the matched route for the current request, if any.
-// Note: this only works when called inside the handler of the matched route
-// because it uses context.Get() which will be cleared after executing the
-// handler.
+// This only works when called inside the handler of the matched route
+// because the matched route is stored in the request context which is cleared
+// after the handler returns, unless the KeepContext option is set on the
+// Router.
 func CurrentRoute(r *http.Request) *Route {
 	if rv := context.Get(r, routeKey); rv != nil {
 		return rv.(*Route)
