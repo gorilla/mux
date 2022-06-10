f6dde0e3977bd569ff843ae6cc71b51ed8194112
selmounik35 <63984216+selmounik35@users.noreply.github.com>
2022-06-10 22:41:15 +0100

    test

--- bench_test.go
+++ bench_test.go
@@ -1,7 +1,7 @@
 // Copyright 2012 The Gorilla Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+//tset
 package mux
 
 import (
